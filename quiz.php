0018d65d9b98c18d29068696c8d20391b6ed3cf0
olamidecode <olaakinde16@gmail.com>
2019-08-18 14:15:17 +0200

    1st commiit

---
+++ quiz.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- bootstrap cdn -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- my style sheet -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+   <!-- font awsome cdn link -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- linear icons cdn -->
+	<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+	<link rel='stylesheet' type='text/css' href='css/style.css'>
+    <title>Document</title>
+</head>
+<body>
+    
+<!-- bootstrap nav-bar -->
+<div class='container-fluid pre-header'>
+		<div class='container'>
+			
+			<p id="big">
+				Free shipping for standard order over R100
+			</p>
+
+			<div class="container-item text-right socials">
+				<i class="fa fa-facebook-f facebook"></i>
+				<i class="fa fa-google-plus google"></i>
+				<i class="fa fa-twitter twitter"></i>
+				<i class="fa fa-pinterest pinterest"></i>
+			</div>
+		</div>
+	</div>
+	<!-- End Top bar -->
+	<!-- Section -->
+	<section class='section section-one container-fluid'>
+		<div class='container'>
+
+			<section>
+				<div class="topnav" id="myTopnav">
+					<a href="index.html" class="active">Home</a>
+					<a href="shop.html" class="active1">Shop</a>
+					<a href="#" class="active2">About</a>
+					<a href="#" class="active3">OLAMIDE</a>
+					<a href="#" class="active4">Blog</a>
+					<a href="#" class="active5">Contact</a>
+					<a href="lookbook.html" class="active6">Lookbook</a>
+					<a href="cart.html"><span class="lnr lnr-cart"></span></a>
+					<a href="javascript:;" class="icon" id='navTrigger'><i class="fa fa-bars"></i></a>
+				</div>
+			</section>
+
+<h1>code space online quiz to test your brain</h1>
+    <?php
+    $example = "hello world";
+    echo $example;
+  echo "Hello world"
+
+    ?>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</html>
