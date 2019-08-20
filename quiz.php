5b4dafc82d2cedd9310ec9c8be71fa0172cad1de
olamidecode <olaakinde16@gmail.com>
2019-08-20 09:57:23 +0200

    changed the code format

--- quiz.php
+++ quiz.php
@@ -49,110 +49,286 @@
 
 <h1 class="intro">Trivia questions to test your brain</h1>
  <form action="quiz.php"method="post">
-
-  <h3>1)HTML stands for?</h3>
-  <input type="radio" name="gender" value="Hypertext Markup Language" > Hypertext Markup Language<br>
-  <input type="radio" name="gender" value="Hyper Markup Language"> Hyper Markup Language<br>
-  <input type="radio" name="gender" value="Hypertext Markup Sign"> Hypertext Markup Sign<br>
-  <input type="radio" name="gender" value="Hypertext Sign Markup"> Hypertext Sign Markup
-<br>
-  <h3>2)what is www?</h3>
-  <input type="radio" name="Internet" value="World WIde Web"> World WIde Web<br>
-  <input type="radio" name="Internet" value="Web Wide World"> Web Wide World<br>
-  <input type="radio" name="Internet" value="Web for the world "> Web for the world<br>
-  <input type="radio" name="Internet" value="other"> Other   
-<br>
-  <h3>3)PHP stands for what?</h3>
-  <input type="radio" name="php" value="PHP Hypertext Preprocessor"> PHP Hypertext Preprocessor<br>
-  <input type="radio" name="php" value="Hypertext Preprocessor"> Hypertext Preprocessor<br>
-  <input type="radio" name="php" value="Preprocessor Hypertext">  Preprocessor Hypertext<br>
-  <input type="radio" name="php" value="PHP Hypertext">  PHP Hypertext
-  <br>
-  <h3>4)What colour jersey is worn by the winners of each stage of the Tour De France? </h3>
-  <input type="radio" name="bike" value="Yellow"> Yellow<br>
-  <input type="radio" name="bike" value="Brown"> Brown<br>
-  <input type="radio" name="bike" value="Blue">  Blue<br>
-  <input type="radio" name="bike" value="Red">  Red
-<br>
-  <h3>5)which boxer has a career of 49 fights without ever having been defeated? </h3>
-  <input type="radio" name="Boxing" value="Jaden Smith"> Jaden Smith<br>
-  <input type="radio" name="Boxing" value="Floyd Mayweather"> Floyd Mayweather<br>
-  <input type="radio" name="Boxing" value="Rocky Marciano"> Rocky Marciano<br>
-  <input type="radio" name="Boxing" value="Mike Tyson"> Mike Tyson
-<br>
-  <h3>6)What chess piece can you move diagonally? </h3>
-  <input type="radio" name="Chess" value="Pawn"> Pawn<br>
-  <input type="radio" name="Chess" value="Knight"> Knight<br>
-  <input type="radio" name="Chess" value="Bishop"> Bishop<br>
-  <input type="radio" name="Chess" value="Castle"> Castle
-<br>
-  <h3>7)What is the diameter of Earth? </h3>
-  <input type="radio" name="Earth" value="20,000 miles"> 20,000 miles<br>
-  <input type="radio" name="Earth" value="11,000 miles"> 11,000 miles<br>
-  <input type="radio" name="Earth" value="9,000 miles"> 9,000 miles<br>
-  <input type="radio" name="Earth" value="8,000 miles"> 8,000 miles
-<br>
-  <h3>8)What is the capital city of Spain? </h3>
-  <input type="radio" name="Place" value="Barcelona"> Barcelona<br>
-  <input type="radio" name="Place" value="Cape Town"> Cape Town<br>
-  <input type="radio" name="Place" value="Celta"> Celta<br>
-  <input type="radio" name="Place" value="Madrid"> Madrid
-<br>
-  <h3>9)How does spider man use his webs? </h3>
-  <input type="radio" name="Hero" value="Web slinger"> Web slinger<br>
-  <input type="radio" name="Hero" value="A spider"> A spider<br>
-  <input type="radio" name="Hero" value="Other"> Other<br>
-  <input type="radio" name="Hero" value="Web gun"> Web gun
-<br>
-  <h3>10)What planet are we on? </h3>
-  <input type="radio" name="Earth" value="Venus"> Venus<br>
-  <input type="radio" name="Earth" value="Pluto"> Pluto<br>
-  <input type="radio" name="Earth" value="Mars"> Mars<br>
-  <input type="radio" name="Earth" value="Earth"> Earth
-<br>
-  <h3>11)Name the seventh planet from the sun? </h3>
-  <input type="radio" name="planet" value="Pluto"> Pluto<br>
-  <input type="radio" name="planet" value="Uranus"> Uranus<br>
-  <input type="radio" name="planet" value="Venus"> Venus<br>
-  <input type="radio" name="planet" value="Neptune"> Neptune
-<br>
-  <h3>12)What team does Messi play for? </h3>
-  <input type="radio" name="Soccer" value="Manchester city"> Manchester city<br>
-  <input type="radio" name="Soccer" value="Real Madrid"> Real Madrid<br>
-  <input type="radio" name="Soccer" value=" Valencia"> Valencia<br>
-  <input type="radio" name="Soccer" value="Barcelona Football Club"> Barcelona Football club
-<br>
-  <h3>13)Whats another name for Barcelona? </h3>
-  <input type="radio" name="Soccer2" value="Barca"> Barca<br>
-  <input type="radio" name="Soccer2" value= "Real Madrid"> Real Madrid<br>
-  <input type="radio" name="Soccer2" value=" Catalonia"> Catalonia<br>
-  <input type="radio" name="Soccer2" value=" Bfc"> Bfc
-<br>
-  <h3>14)Which is the largest desert on earth? </h3>
-  <input type="radio" name="desert" value="Sonoran"> Sonoran<br>
-  <input type="radio" name="desert" value= "Sahara"> Sahara<br>
-  <input type="radio" name="desert" value=" kalahari"> kalahari<br>
-  <input type="radio" name="desert" value=" Arabian">Arabian
+ <div id="page-wrap">
  
+<form action="result.php" method="post" id="quiz">
+ 
+         <ol>
+         
+             <li>
+             
+                 <h3>HTML stands for?</h3>
+                 
+                 <div>
+                     <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                     <label for="question-1-answers-A">A)Hypertext Markup Language </label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                     <label for="question-1-answers-B">B)Hyper Markup Language</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                     <label for="question-1-answers-C">C) Hypertext Markup Sign</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                     <label for="question-1-answers-D">D) Hypertext Sign Markup</label>
+                 </div>
+             
+             </li>
+             
+             <li>
+             
+                 <h3> what is www?</h3>
+                 
+                 <div>
+                     <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                     <label for="question-2-answers-A">A) World WIde Web"> World WIde Web</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                     <label for="question-2-answers-B">B) Web Wide World"> Web Wide World</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                     <label for="question-2-answers-C">C) Web for the world "> Web for the world</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+                     <label for="question-2-answers-D">D) Other</label>
+                 </div>
+             
+             </li>
+             
+             <li>
+             
+                 <h3>PHP stands for what?</h3>
+                 
+                 <div>
+                     <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                     <label for="question-3-answers-A">A) PHP Hypertext Preprocessor</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                     <label for="question-3-answers-B">B) Hypertext Preprocessor</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                     <label for="question-3-answers-C">C) Preprocessor Hypertext</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+                     <label for="question-3-answers-D">D) PHP Hypertext</label>
+                 </div>
+             
+             </li>
+             
+             <li>
+             
+                 <h3>What colour jersey is worn by the winners of each stage of the Tour De France?</h3>
+                 
+                 <div>
+                     <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+                     <label for="question-4-answers-A">A) Yellow</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                     <label for="question-4-answers-B">B) Brown</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                     <label for="question-4-answers-C">C) Blue</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
+                     <label for="question-4-answers-D">D) Red</label>
+                 </div>
+             
+             </li>
+             
+             <li>
+             
+                 <h3>Which boxer has a career of 49 fights without ever having been defeated? </h3>
+                 
+                 <div>
+                     <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                     <label for="question-5-answers-A">A)Jaden Smith</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                     <label for="question-5-answers-B">B) Floyd Mayweather</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                     <label for="question-5-answers-C">C)Rocky Marciano</label>
+                 </div>
+                 
+                 <div>
+                     <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                     <label for="question-5-answers-D">D) Mike Tyson</label>
+                 </div>
+             
+             </li>
+
+             <li>
+             
+             <h3>Who`s parents did Bucki (The winter shoulder) kill ?</h3>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                 <label for="question-5-answers-A">A) Captain America</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                 <label for="question-5-answers-B">B) Black-widow</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                 <label for="question-5-answers-C">C) Spider-man</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                 <label for="question-5-answers-D">D) Iron-man</label>
+             </div>
+         
+         </li>
+
+         <li>
+             
+             <h3>Who`s parents did Bucki (The winter shoulder) kill ?</h3>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                 <label for="question-5-answers-A">A) Captain America</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                 <label for="question-5-answers-B">B) Black-widow</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                 <label for="question-5-answers-C">C) Spider-man</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                 <label for="question-5-answers-D">D) Iron-man</label>
+             </div>
+         
+         </li>
+
+         <li>
+             
+             <h3>Who`s parents did Bucki (The winter shoulder) kill ?</h3>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                 <label for="question-5-answers-A">A) Captain America</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                 <label for="question-5-answers-B">B) Black-widow</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                 <label for="question-5-answers-C">C) Spider-man</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                 <label for="question-5-answers-D">D) Iron-man</label>
+             </div>
+         
+         </li>
+
+         <li>
+             
+             <h3>Who`s parents did Bucki (The winter shoulder) kill ?</h3>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                 <label for="question-5-answers-A">A) Captain America</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                 <label for="question-5-answers-B">B) Black-widow</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                 <label for="question-5-answers-C">C) Spider-man</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                 <label for="question-5-answers-D">D) Iron-man</label>
+             </div>
+         
+         </li>
 
 
+         <li>
+             
+             <h3>Who`s parents did Bucki (The winter shoulder) kill ?</h3>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                 <label for="question-5-answers-A">A) Captain America</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                 <label for="question-5-answers-B">B) Black-widow</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                 <label for="question-5-answers-C">C) Spider-man</label>
+             </div>
+             
+             <div>
+                 <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                 <label for="question-5-answers-D">D) Iron-man</label>
+             </div>
+         
+         </li>
+         
+         </ol>
+         
+         <!-- <input type="submit" value="Submit" class="btn" /> -->
+         <div id="btn">
+         <button type="submit" value="Submit" class="btn">SUBMIT</button>
+         </div>
+ 
+ </form>
 
+</div>
 
- <br>
-
- <br>
-
-     <input type="submit">
-</form>
-
-    <!-- <?php
-    $example = "hello world";
-    echo $example;
-  echo "Hello world"
-
-    ?>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</html> -->
+</html> 
